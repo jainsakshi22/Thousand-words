82bce74c95fe805b422d09cb9e20f0c140c5fbef
jainsakshi22 <jainsakshi22@gmail.com>
2014-11-12 20:31:54 +0530

    Created project

---
+++ Thousand Words/TWAlbumTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  TWAlbumTableViewController.h
+//  Thousand Words
+//
+//  Created by Sakshi Jain on 11/11/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TWAlbumTableViewController : UITableViewController
+
+@property (strong,nonatomic) NSMutableArray *albums;
+
+@end
