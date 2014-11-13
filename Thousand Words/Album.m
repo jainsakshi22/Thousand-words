f630db0a6f91097750f55ac62b72fddfdc0e6c24
jainsakshi22 <jainsakshi22@gmail.com>
2014-11-13 21:03:56 +0530

    Updating our xdataModel

---
+++ Thousand Words/Album.m
@@ -0,0 +1,17 @@
+//
+//  Album.m
+//  Thousand Words
+//
+//  Created by Sakshi Jain on 13/11/14.
+//
+//
+
+#import "Album.h"
+
+
+@implementation Album
+
+@dynamic name;
+@dynamic date;
+
+@end
--- Thousand Words/TWAlbumTableViewController.m
+++ Thousand Words/TWAlbumTableViewController.m
@@ -37,8 +37,8 @@
 
 #pragma mark - Table view data source
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{    
     return 1;
 }
 
