e39ed605417cebcbca897c2513a7f1102b2d698e
jainsakshi22 <jainsakshi22@gmail.com>
2014-12-20 18:24:05 +0530

    Adding a collectionViewController for the Filters

---
+++ Thousand Words/FiltersCollectionViewController.h
@@ -0,0 +1,17 @@
+//
+//  FiltersCollectionViewController.h
+//  Thousand Words
+//
+//  Created by Sakshi Jain on 20/12/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@class Photo;
+
+@interface FiltersCollectionViewController : UICollectionViewController
+
+@property(strong,nonatomic) Photo *photo;
+
+@end
--- Thousand Words/PhotoDetailViewController.m
+++ Thousand Words/PhotoDetailViewController.m
@@ -8,6 +8,7 @@
 
 #import "PhotoDetailViewController.h"
 #import "Photo.h"
+#import "FiltersCollectionViewController.h"
 
 @interface PhotoDetailViewController ()
 
@@ -32,15 +33,22 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
+
 #pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if ([segue.identifier isEqualToString:@"Filter Segue"])
+    {
+        if ([segue.destinationViewController isKindOfClass:[FiltersCollectionViewController class]])
+        {
+            FiltersCollectionViewController *targetViewController = segue.destinationViewController;
+            targetViewController.photo = self.photo;
+        }
+    }
+    
 }
-*/
+
 
 #pragma mark - IBActions method
 
