83cac2840dd29a8c53ffa97c67b9932f08d8eb06
jainsakshi22 <jainsakshi22@gmail.com>
2014-12-27 15:58:44 +0530

    Saving our filters

--- Thousand Words/FiltersCollectionViewController.m
+++ Thousand Words/FiltersCollectionViewController.m
@@ -123,6 +123,20 @@ static NSString * const reuseIdentifier = @"Photo Cell";
 
 #pragma mark <UICollectionViewDelegate>
 
+-(void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    TWPhotoCollectionViewCell *selectedCell = (TWPhotoCollectionViewCell *) [collectionView cellForItemAtIndexPath:indexPath];
+    self.photo.image = selectedCell.imageView.image;
+    
+    NSError *error = nil;
+    if (![[self.photo managedObjectContext] save:&error])
+    {
+        NSLog(@"error");
+    }
+    
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
 /*
 // Uncomment this method to specify if the specified item should be highlighted during tracking
 - (BOOL)collectionView:(UICollectionView *)collectionView shouldHighlightItemAtIndexPath:(NSIndexPath *)indexPath {
